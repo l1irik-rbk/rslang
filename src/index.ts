021d891e03f696b1a01f36527ab6998c50cbdadc
l1irik-rbk <kir.baranow2010@gmail.com>
2022-02-20 19:59:28 +0300

    fix: textbook bags

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { router } from './app';
 import './bootstrap/bootstrap.min.css';
-import { setPageAndGroup } from './views/components/textbook/savePages';
+import { getPageAndGroup, setPageAndGroup } from './views/components/textbook/savePages';
+
+window.addEventListener('beforeunload', setPageAndGroup);
+window.addEventListener('load', getPageAndGroup);
 
 // Listen on hash change:
 window.addEventListener('hashchange', router);
 
 // Listen on page load:
 window.addEventListener('load', router);
-
-window.addEventListener('beforeunload', setPageAndGroup);
